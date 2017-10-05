13eaaaab4f369784e72337a4423eccf2c29b49ec
hmelo1 <heriberto@melo.nyc>
2017-10-05 02:29:09 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -7,7 +7,6 @@ Create a function removeFromPlaylist that accepts two arguments (the playlist ob
 
 var playlist = { artist1: "song1" }
 function updatePlaylist(playlist, artistName, songTitle){
-  var = artistName
-  //playlist[artistName]= 'songTitle'
+
   return playlist
 }
